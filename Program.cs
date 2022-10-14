4d5311f8f3c7d6884d3fe94c22d32352b713b2b9
Semen <semgenomsk@yandex.ru>
2022-10-14 21:42:11 +0600

    Done Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая из имеюегося массива строк формирует массив из строк, 
+//длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+//лучше обойтись исключительно массивами
+
+string[] arr = { "Hello", "World", "qq", "a" };
+Console.WriteLine(string.Join(", ", arr));
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i].Length <= 3)
+    {
+        Console.Write(arr[i] + "  ");
+    }
+}
